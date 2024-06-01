5c165eda2acc56fca610b5b2686f0ffcf88976d0
sim <sim@advte.com>
2024-06-01 11:08:33 -0400

    Swap red/blue channels

--- main.cpp
+++ main.cpp
@@ -11,54 +11,37 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-uint8_t* ImageFromDisplay(int& Width, int& Height, int& BytesPerPixel)
+void ScreenCapture()
 {
-   uint8_t* image_data = nullptr;
-   uint64_t image_size = 0;
-
    Display* display = XOpenDisplay(":1");
    Window root = DefaultRootWindow(display);
 
    XWindowAttributes attributes = {0};
    XGetWindowAttributes(display, root, &attributes);
 
-   if (Width > attributes.width)
-      Width = attributes.width;
-
-   if (Height > attributes.height)
-      Height = attributes.height;
-
-   printf(">>> get image %dx%d\n", Width, Height);
-   XImage* img = XGetImage(display, root, 0, 0 , Width, Height, AllPlanes, ZPixmap);
-   printf(">>> got image\n");
+   XImage* img = XGetImage(display, root, 0, 0 , attributes.width, attributes.height, AllPlanes, ZPixmap);
 
    if (img)
    {
-      BytesPerPixel = img->bits_per_pixel / 8;
+      int bytes_per_pixel = img->bits_per_pixel / 8;
 
-      image_size = Width * Height * BytesPerPixel;
-      image_data = new uint8_t[image_size];
+      // swap red and blue channels
+      for (int i = 0; i < (attributes.width * attributes.height * bytes_per_pixel); i += bytes_per_pixel)
+      {
+         uint8_t tmp = img->data[i];
+         img->data[i] = img->data[i+2];
+         img->data[i+2] = tmp;
+      }
 
-      printf(">>> copy image: %d bytes\n", image_size);
-      memcpy(image_data, img->data, image_size);
+      stbi_write_png("test.png", attributes.width, attributes.height, bytes_per_pixel, img->data, 0);
    
-      printf(">>> destroy image\n");
       XDestroyImage(img);
    }
 
    XCloseDisplay(display);
-
-   return image_data;
 }
 
 int main()
 {
-   int      width = 4096;
-   int      height = 4096;
-   int      bytes_per_pixel = 0;
-
-   uint8_t* img = ImageFromDisplay(width, height, bytes_per_pixel);
-
-   printf(">>> write to png\n");
-   stbi_write_png("test.png", width, height, bytes_per_pixel, img, 0);
+   ScreenCapture();
 }
